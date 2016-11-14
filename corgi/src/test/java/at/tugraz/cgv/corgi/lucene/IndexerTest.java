3ff97c06a97907dd775af448166a9694a7ec0a97
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2016-11-14 17:26:03 +0100

    Index impl

---
+++ corgi/src/test/java/at/tugraz/cgv/corgi/lucene/IndexerTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.tugraz.cgv.corgi.lucene;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Test;
+
+/**
+ *
+ * @author heinz
+ */
+public class IndexerTest {
+
+  public IndexerTest() {
+  }
+
+  @Test
+  public void testCreateNewIndex() {
+    try {
+      Path tempIndex = Files.createTempDirectory("corgi_");
+      System.out.println("Temp Index Dir: " + tempIndex.toString());
+      Indexer indexer = new Indexer(tempIndex.toString(), true);
+      File resourcesDirectory = new File("src/test/resources");
+      indexer.index(resourcesDirectory.getAbsolutePath());
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+}
