e1c2ddc96fc0b6318defc36a591a19048a16db1a
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2016-11-14 13:45:36 +0100

    Maven project initial

---
+++ corgi/src/main/java/at/tugraz/cgv/corgi/SampleClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.tugraz.cgv.corgi;
+
+/**
+ *
+ * @author heinz
+ */
+public class SampleClass {
+  
+  public static int getSome() {
+    return 4;
+  }
+  
+}
