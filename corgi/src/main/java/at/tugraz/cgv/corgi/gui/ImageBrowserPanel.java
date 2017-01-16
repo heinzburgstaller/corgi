2edcfbb7fa288e79049e0099a37d1c1b08b0624d
Heinz Burgstaller <heinz.burgstaller@student.tugraz.at>
2017-01-16 13:16:31 +0100

    New stuff 2

--- corgi/src/main/java/at/tugraz/cgv/corgi/gui/ImageBrowserPanel.java
+++ corgi/src/main/java/at/tugraz/cgv/corgi/gui/ImageBrowserPanel.java
@@ -9,9 +9,13 @@ import at.tugraz.cgv.corgi.gui.model.ImageItem;
 import at.tugraz.cgv.corgi.lucene.Searcher;
 import at.tugraz.cgv.corgi.util.PropertyLoader;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -21,26 +25,82 @@ import org.apache.lucene.queryparser.classic.ParseException;
  * @author heinz
  */
 public class ImageBrowserPanel extends JPanel {
-  
+
+  private static final int IMAGES_PER_PAGE = 16;
+  private int currentPage = 0;
+  private int maxPages;
+
+  private JPanel navPanel;
+  private List<ImageItem> images = new ArrayList<>();
   private final ImageBrowser imageBrowser;
   Searcher searcher;
-  
+  private JButton btnFirst = new JButton("<<");
+  private JButton btnLast = new JButton(">>");
+  private JButton btnNext = new JButton(">");
+  private JButton btnPrevious = new JButton("<");
+  private JLabel navLabel = new JLabel();
+
   public ImageBrowserPanel() {
     super(new BorderLayout());
-    
+
+    navPanel = new JPanel();
+    navPanel.setLayout(new BoxLayout(navPanel, BoxLayout.X_AXIS));
+    navPanel.add(btnFirst);
+    navPanel.add(btnPrevious);
+    navPanel.add(navLabel);
+    navPanel.add(btnNext);
+    navPanel.add(btnLast);
+
     searcher = new Searcher(PropertyLoader.getIndexPath());
-    List<ImageItem> images = new ArrayList<>();
     try {
       images = searcher.findAllImages();
+      maxPages = images.size() / IMAGES_PER_PAGE;
     } catch (IOException | ParseException ex) {
       throw new RuntimeException(ex);
     }
-    
+
     imageBrowser = new ImageBrowser();
-    imageBrowser.addImages(images.subList(0, 20));
+    navigate();
     add(new JScrollPane(imageBrowser), BorderLayout.CENTER);
-    
+    add(navPanel, BorderLayout.SOUTH);
+
     this.validate();
     this.repaint();
+
+    btnNext.addActionListener((ActionEvent e) -> {
+      if (currentPage < maxPages) {
+        currentPage++;
+        navigate();
+      }
+    });
+
+    btnPrevious.addActionListener((ActionEvent e) -> {
+      if (currentPage > 0) {
+        currentPage--;
+        navigate();
+      }
+    });
+
+    btnFirst.addActionListener((ActionEvent e) -> {
+      currentPage = 0;
+      navigate();
+    });
+
+    btnLast.addActionListener((ActionEvent e) -> {
+      currentPage = maxPages;
+      navigate();
+    });
+  }
+
+  private void navigate() {
+    int from = currentPage * IMAGES_PER_PAGE;
+    int to = currentPage * IMAGES_PER_PAGE + IMAGES_PER_PAGE;
+    if (to > images.size()) {
+      to = images.size();
+    }
+
+    imageBrowser.addImages(images.subList(from, to));
+
+    navLabel.setText("  Page " + (currentPage + 1) + " of " + (maxPages + 1) + "  ");
   }
 }
